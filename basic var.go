9f944a3161db307e1fe1414e4d8b14d73570597f
Aiyappan-hub <63788802+Aiyappan-hub@users.noreply.github.com>
2020-06-04 07:07:30 -0700

    Create basic var.go

---
+++ basic var.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    var a = "kloudone"
+    fmt.Println(a)
+
+    var b = "IT"
+    fmt.Println(b)
+
+    var c = "california"
+    fmt.Println(c)
+
+}
